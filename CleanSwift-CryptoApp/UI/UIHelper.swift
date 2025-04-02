e21b90cc52b76e37e5278700bbe6fa7ad287b10c
Nikita Filonov <iamnikitafilonov@ggmail.com>
2025-04-02 22:40:14 +0500

    Added UIHelper with base values

---
+++ CleanSwift-CryptoApp/UI/UIHelper.swift
@@ -0,0 +1,33 @@
+//
+//  UIHelper.swift
+//  CleanSwift-CryptoApp
+//
+//  Created by Nikita Filonov on 02.04.2025.
+//
+
+import Foundation
+import UIKit
+
+class UIHelper {
+    
+    // MARK: - Margins
+    
+    enum Margins {
+        static let smallOffset: CGFloat = 2
+        static let halfOffset: CGFloat = 4
+        static let offset: CGFloat = 8
+        static let mediumOffset: CGFloat = 12
+        static let doubleOffset: CGFloat = 16
+        static let bigOffset: CGFloat = 20
+        static let tripleOffset: CGFloat = 24
+        static let quadrupleOffset: CGFloat = 32
+    }
+    
+    // MARK: - Animations
+    
+    enum Animations {
+        static let duration: Double = 0.25
+        static let doubleDuration: Double = 0.5
+        static let longDelay: Double = 1
+    }
+}
